3093907261374b6e0631cef7edf36f46c15adfec
author <author@example.com>
2022-04-15 16:37:28 +0900

    .

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -202,7 +202,7 @@
     return MAX - score;
   }
 
-  function typewriter(node, { speed = 20 }) {
+  function typewriter(node, { speed = 30 }) {
     const text = 'Loading...';
     const duration = 1000 * text.length / speed;
 
