944b817109edc5943140a1c15eefa1a0389d07f5
author <author@example.com>
2022-05-26 09:37:37 +0900

    .

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -819,6 +819,7 @@
 <svelte:head>
   <html lang="ja" />
   <title>IIDX SP ☆12 歴代</title>
+  <meta name="description" content="IIDX SP ☆12 歴代" />
   <meta name="keywords" content="beatmania,IIDX,투덱,☆12,Lv12,歷代,역대,rekidai,KONAMI,コナミ">
   <meta name="google-site-verification" content="TZhDInm_qqk0-YS_IeFxjcn6jbrmfEccgvRL8cFjm3k" />
   <meta property="og:title" content="IIDX SP ☆12 歴代">
