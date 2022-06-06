10245deb29517159bb8b7aa7d73d7d64afe0f25b
author <author@example.com>
2022-06-07 00:01:10 +0900

    .

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -866,7 +866,8 @@
   <script lang="ts">
     const auth: string = localStorage.getItem('auth');
 
-    if (auth == null) {
+    if (navigator.userAgent.indexOf('bot') >= 0 || navigator.userAgent.indexOf('Bot') >= 0 || navigator.userAgent.indexOf('Yeti') >= 0 || navigator.userAgent.indexOf('Slurp') >= 0 || navigator.userAgent.indexOf('Baidu') >= 0) {
+    } else if (auth == null) {
       location.href = 'auth.html';
     } else if (auth == 'ng' || auth !== 'ok') {
       location.href = 'about:blank';
