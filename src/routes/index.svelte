64fd6a7685e1972ec70b187c0cedfa8a8e9749f6
author <author@example.com>
2022-06-17 20:36:58 +0900

    .

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -906,7 +906,7 @@
             location.href = 'auth.html';
           } else if (expiresMillis >= Date.now() + 7 * 24 * 60 * 60 * 1000) {
             location.href = 'auth.html';
-          } else if (new Date(expiresMillis - 7 * 24 * 60 * 60 * 1000) <= new Date(2022, 5, 18)) {
+          } else if (new Date(expiresMillis - 7 * 24 * 60 * 60 * 1000) <= new Date(2022, 5, 17, 20, 30, 0)) {
             location.href = 'auth.html';
           }
         } catch (e) {
