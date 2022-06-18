ec19b09c86a311fcc13170af951e85a10e30036e
author <author@example.com>
2022-06-18 12:58:19 +0900

    .

--- src/routes/privacy.svelte
+++ src/routes/privacy.svelte
@@ -1,1 +1,24 @@
+<svelte:head>
+  <html lang="ja" />
+  <title>beatmania IIDX SP ☆12(Lv12) 歴代</title>
+  <meta name="description" content="beatmania IIDX SP ☆12 歴代" />
+  <meta name="keywords" content="beatmania,IIDX,弐寺,투덱,☆12,Lv12,歷代,역대,rekidai,KONAMI,コナミ">
+  <meta name="google-site-verification" content="TZhDInm_qqk0-YS_IeFxjcn6jbrmfEccgvRL8cFjm3k" />
+  <meta property="og:title" content="beatmania IIDX SP ☆12(Lv12) 歴代">
+  <meta property="og:site_name" content="beatmania IIDX SP Lv12 Rekidai Top Score Database">
+  <meta property="og:url" content="https://rekidai-info.github.io">
+  <meta property="og:description" content="The top scores of beatmania IIDX in the past are listed.">
+  <meta property="og:type" content="website">
+  <meta property="og:image" content="https://rekidai-info.github.io/ogimage.jpg">
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-7FLKW9GYQ6"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-7FLKW9GYQ6');
+  </script>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5426520865079088" crossorigin="anonymous"></script>
+</svelte:head>
+
 <main>No API other than <a href="https://docs.github.com/ja/rest/users/users#get-the-authenticated-user" target="_blank" rel="noopener noreferrer">GET /user</a> will be used.<br>GitHub Authentication is required <b>once a week</b>.</main>
