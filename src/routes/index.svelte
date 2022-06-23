1d7d2acc0321622c423a080436f41031c7150184
author <author@example.com>
2022-06-24 07:50:24 +0900

    .

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -867,8 +867,8 @@
 <svelte:head>
   <html lang="ja" />
   <title>beatmania IIDX SP ☆12(Lv12) 歴代全一 Top Scores/Rankers Viewer</title>
-  <meta name="description" content="beatmania IIDX(弐寺/투덱) SP ☆12(Lv12) 歴代全一(역대)スコア(Score)/ランカー(Top Ranker)を表示します。" />
-  <meta name="keywords" content="beatmania,IIDX,弐寺,투덱,☆12,Lv12,歷代,歴代全一,歴代スコア,역대,TOP RANKER,TOP RANKERS,rekidai,KONAMI,コナミ">
+  <meta name="description" content="beatmania IIDX(弐寺/투덱) SP ☆12(Lv12) 歴代全一(역대/Rekidai)スコア(Score)/ランカー(Top Ranker)を表示します。" />
+  <meta name="keywords" content="beatmania,IIDX,iidx,弐寺,투덱,☆12,Lv12,歷代,歴代全一,歴代スコア,역대,TOP RANKER,TOP RANKERS,rekidai,KONAMI,コナミ">
   <meta name="google-site-verification" content="TZhDInm_qqk0-YS_IeFxjcn6jbrmfEccgvRL8cFjm3k" />
   <meta property="og:title" content="beatmania IIDX SP ☆12(Lv12) 歴代">
   <meta property="og:site_name" content="beatmania IIDX SP Lv12 Rekidai Top Score Database">
