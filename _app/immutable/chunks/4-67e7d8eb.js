6c2da6f5406262d43df95bb4100cf0fa56a79ec2
workflow <workflow@example.com>
2024-12-21 07:05:05 +0000

    Automatically updated by Workflow Action

---
+++ _app/immutable/chunks/4-67e7d8eb.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/index/_page.svelte-1336d763.js";export{t as component};
--- _app/immutable/chunks/4-c42d79cf.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/index/_page.svelte-1793c849.js";export{t as component};
