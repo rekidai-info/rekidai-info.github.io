fe13aa380ef3d551cbbd88c1d43bd17554661e13
workflow <workflow@example.com>
2025-01-13 22:22:08 +0000

    Automatically updated by Workflow Action

---
+++ _app/immutable/chunks/4-03c4f8d8.js
@@ -0,0 +1,4 @@
+import { default as default2 } from "../components/pages/index/_page.svelte-f00f7c72.js";
+export {
+  default2 as component
+};
--- _app/immutable/chunks/4-c49d5df1.js
+++
@@ -1,4 +0,0 @@
-import { default as default2 } from "../components/pages/index/_page.svelte-dca4e182.js";
-export {
-  default2 as component
-};
